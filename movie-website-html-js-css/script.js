966c5079045f8807def6b6a207c342209c6f231a
Münevver Yıldırım <115148273+mnevveryild@users.noreply.github.com>
2026-03-30 20:23:02 +0000

    eklendi

---
+++ movie-website-html-js-css/script.js
@@ -0,0 +1,38 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i) => {
+    let clickCounter = 0;
+    const imageItem = movieLists[i].querySelectorAll("img").length;
+
+    
+    arrow.addEventListener("click", function() {
+    clickCounter++;
+    if(imageItem - (4+ clickCounter) >= 0){
+        
+        movieLists[i].style.transform = `translateX(${movieLists[i].
+        computedStyleMap().get("transform")[0].x.value-300}px)`;
+        }
+
+    else{
+
+    movieLists[i].style.transform = "translateX(0)";
+
+    }
+   
+});
+});
+
+
+/*dark-mode*/
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.movie-list-filter,.movie-item-buttons i,.movie-item-buttons,.movie-item-title,.select,.toggle-ball,.toggle,.toggle-icon,.left-menu-icon,.left-menu-icon i,.left-menu,.left-menu ul li a,.sidebar,.sidebar i,.sidebar ul li a,.navbar,.navbar i,.navbar ul li a,.movie-list-filter select,.movie-list-filter");
+ball.addEventListener("click", function() {
+    
+    items.forEach(item => {
+        item.classList.toggle("active");
+    })
+})
+
+console.log(window.innerWidth);
